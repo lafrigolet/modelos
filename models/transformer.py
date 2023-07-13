89a2f435dc83eeba069bd7bcb323a08894e84198
Luis A Frigolet <lafrigolet@gmail.com>
2023-07-13 17:43:03 +0200

    Moved train and eval function to TransformerModel class as methods

--- models/transformer.py
+++ models/transformer.py
@@ -51,7 +51,52 @@ class TransformerModel(nn.Module):
         output = self.linear(output)
         return output
 
+    # def train(model: nn.Module, criterion, lr, optimizer, scheduler) -> None:
+    def train(self, criterion, lr, optimizer, scheduler) -> None:
+        super(TransformerModel, self).train()  # turn on train mode
+        total_loss = 0.
+        log_interval = 200
+        start_time = time.time()
+        
+        num_batches = len(train_data) // bptt
+        for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+            data, targets = get_batch(train_data, i)
+            output = self(data)
+            output_flat = output.view(-1, ntokens)
+            loss = criterion(output_flat, targets)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.parameters(), 0.5)
+            optimizer.step()
+            
+            total_loss += loss.item()
+            if batch % log_interval == 0 and batch > 0:
+                lr = scheduler.get_last_lr()[0]
+                ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+                cur_loss = total_loss / log_interval
+                ppl = math.exp(cur_loss)
+                print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                      f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                      f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+                total_loss = 0
+                start_time = time.time()
 
+
+    #def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+    def evaluate(self, eval_data: Tensor) -> float:
+        self.eval()  # turn on evaluation mode
+        total_loss = 0.
+        with torch.no_grad():
+            for i in range(0, eval_data.size(0) - 1, bptt):
+                data, targets = get_batch(eval_data, i)
+                seq_len = data.size(0)
+                output = self(data)
+                output_flat = output.view(-1, ntokens)
+                total_loss += seq_len * criterion(output_flat, targets).item()
+        return total_loss / (len(eval_data) - 1)
+
+                
 
 class PositionalEncoding(nn.Module):
 
@@ -155,51 +200,6 @@ criterion = nn.CrossEntropyLoss()
 lr = 5.0  # learning rate
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-
-def train(model: nn.Module) -> None:
-    model.train()  # turn on train mode
-    total_loss = 0.
-    log_interval = 200
-    start_time = time.time()
-    
-    num_batches = len(train_data) // bptt
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
-        data, targets = get_batch(train_data, i)
-        output = model(data)
-        output_flat = output.view(-1, ntokens)
-        loss = criterion(output_flat, targets)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-        optimizer.step()
-        
-        total_loss += loss.item()
-        if batch % log_interval == 0 and batch > 0:
-            lr = scheduler.get_last_lr()[0]
-            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
-            cur_loss = total_loss / log_interval
-            ppl = math.exp(cur_loss)
-            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
-                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
-                f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
-            total_loss = 0
-            start_time = time.time()
-            
-
-def evaluate(model: nn.Module, eval_data: Tensor) -> float:
-    model.eval()  # turn on evaluation mode
-    total_loss = 0.
-    with torch.no_grad():
-        for i in range(0, eval_data.size(0) - 1, bptt):
-            data, targets = get_batch(eval_data, i)
-            seq_len = data.size(0)
-            output = model(data)
-            output_flat = output.view(-1, ntokens)
-            total_loss += seq_len * criterion(output_flat, targets).item()
-    return total_loss / (len(eval_data) - 1)
-
-            
 best_val_loss = float('inf')
 epochs = 3
 
@@ -208,7 +208,7 @@ with TemporaryDirectory() as tempdir:
 
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        train(model)
+        model.train(criterion, lr, optimizer, scheduler)
         val_loss = evaluate(model, val_data)
         val_ppl = math.exp(val_loss)
         elapsed = time.time() - epoch_start_time
@@ -222,4 +222,5 @@ with TemporaryDirectory() as tempdir:
             torch.save(model.state_dict(), best_model_params_path)
 
         scheduler.step()
+        
     model.load_state_dict(torch.load(best_model_params_path)) # load best model states
