b2e652e5818e8619e8182bdd623b9bdf97e7a36b
Luis A Frigolet <lafrigolet@gmail.com>
2023-08-20 08:12:53 +0200

    Solved bug while calculating test Accuracy

--- models/fnn.py
+++ models/fnn.py
@@ -10,7 +10,7 @@ import torch
 import copy
 
 class FNNModel(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes, learning_rate):
+    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
         super(FNNModel, self).__init__()
         self.layer1 = nn.Linear(input_size, hidden_size1)
         self.relu1 = nn.ReLU()
@@ -40,6 +40,10 @@ class FNNModel(nn.Module):
         out = self.output_layer(out)
         out = self.softmax(out)
         return out
+
+
+    def load(self, pth):
+        self.load_state_dict(torch.load(pth))
 
 
     def save(self, output_pth_file):
@@ -95,9 +99,10 @@ class FNNModel(nn.Module):
     
         with torch.no_grad():
             for inputs, labels in test_loader:
+                print(type(inputs), inputs.shape)
                 outputs = self(inputs)
                 _, predicted = torch.max(outputs, 1)
-                _, targets = torch.max(outputs, 1)
+                _, targets = torch.max(labels, 1)
                 correct += (predicted == targets).sum().item()
     
         return correct
@@ -124,12 +129,12 @@ class FNNModel(nn.Module):
 
 # Define your custom dataset class
 class CustomDataset(Dataset):
-    def __init__(self, data, context_size, vocab_len):
+    def __init__(self, data, context_size, vocab_size):
         self.data = data
         self.data.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
         self.len  = len(data) - context_size
         self.context_size = context_size
-        self.vocab_len = vocab_len
+        self.vocab_size = vocab_size
     
     def __len__(self):
         return self.len
@@ -137,7 +142,7 @@ class CustomDataset(Dataset):
     def __getitem__(self, index):
         #data = self.data[index:index + self.context_size]
         data = self.data.narrow(0, index, self.context_size)
-        labels = torch.FloatTensor(self.vocab_len).fill_(0)
+        labels = torch.FloatTensor(self.vocab_size).fill_(0)
         next_token = self.data[index + self.context_size]
         #print(data.to(torch.int), next_token)
         labels[int(next_token)] = 1.
@@ -171,10 +176,11 @@ def train(model, train_loader, test_loader, n_epochs):
                 100. * correct / len(train_loader.dataset)))
 """
 
-def train():
+def load_embeddings():
     # Load Text Processing Embeddings ############################################################
     
     train_iter = WikiText2(root='data/wikitext-2', split='train')
+    print(type(train_iter))
 
     """
     for item in train_iter:
@@ -184,9 +190,9 @@ def train():
     """
 
     tokenizer = get_tokenizer('basic_english')
-    tokenized_train_iter = map(tokenizer, train_iter)
-
+    
     """
+    tokenized_train_iter = map(tokenizer, train_iter)
     for token in tokenized_train_iter:
     print(token)
     """
@@ -218,6 +224,12 @@ index_of_unk = vocab['<unk>']
 print(f"Index of <unk>: {index_of_unk}")
     """
 
+    return tokenizer, vocab
+
+def train():
+
+    tokenizer, vocab = load_embeddings()
+
     def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
         """Converts raw text into a flat Tensor."""
         data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.float32) for item in raw_text_iter]
@@ -241,9 +253,6 @@ print(f"Index of <unk>: {index_of_unk}")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-
-
-
     # Prepare Train Data Set ############################################################
 
     # Example data and labels
@@ -254,13 +263,12 @@ print(f"Index of <unk>: {index_of_unk}")
     vocab_size = len(vocab) # len of vocab
 
     #train_data = train_data[0:1000] # Reduce the train_data set
-    vocab_len = len(vocab)
     train_data_len = len(train_data)
 
     """
 TO DELETE
 data = torch.FloatTensor(train_data_len - context_size, context_size).fill_(0)
-labels = torch.FloatTensor(train_data_len - context_size, vocab_len).fill_(0)
+labels = torch.FloatTensor(train_data_len - context_size, vocab_size).fill_(0)
 
 for i in range(train_data_len - context_size):
     data[i] = train_data[i:i + context_size]
@@ -273,8 +281,8 @@ print(len(labels))
 #print(labels[55])
     """
     # Instantiate your custom dataset
-    train_custom_dataset = CustomDataset(train_data, context_size, vocab_len)
-    test_custom_dataset = CustomDataset(train_data, context_size, vocab_len)
+    train_custom_dataset = CustomDataset(train_data, context_size, vocab_size)
+    test_custom_dataset = CustomDataset(test_data, context_size, vocab_size)
 
     # Create a DataLoader
     batch_size = 64  # Number of samples in each batch
@@ -294,12 +302,78 @@ print(len(labels))
 
     print(f"Hiperparameters: context_size {context_size}, learning_rate {learning_rate}, batch_size {batch_size}, n_epochs {n_epochs}");
     # Instantiate the model
-    model = FNNModel(input_size, hidden_size1, hidden_size2, num_classes, learning_rate)
+    model = FNNModel(input_size, hidden_size1, hidden_size2, num_classes)
     print(model)
 
     model.train_model(train_loader, test_loader, n_epochs, learning_rate)
     model.save('fnn')
 
+class TokensDataSet(Dataset):
+    def __init__(self, data):
+        self.data = data
+#        self.data.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+        self.len = len(data)
+        
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, index):
+        return self.data[index]
+        
+    
+def chat():
+    tokenizer, vocab = load_embeddings()
 
-train()
+    def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+        """Converts raw text into a flat Tensor."""
+        data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.float32) for item in raw_text_iter]
+        return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+    vocab_size = len(vocab)
+    context_size = 100 # number of tokens to use as context
+
+    tokens  = ['<unk>' for _ in range(context_size)]
+    tokens = (tokens + 'Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. Once upon a time in hollywood, a man found a lover and the reason why you should keep searching is that the lover is always there, just searching for you, so dont hesitate and be patience. Luck helps to those keeping patience. '.lower().split())[-context_size:]
+
+    print(tokens, len(tokens))
+
+    tokens_iter = TokensDataSet(tokens)
+    tokens_data = data_process(tokens_iter)[-context_size:]
+
+    print(tokens_data, len(tokens_data))
+
+    input_size =  context_size  # Specify the input size or number of features
+    hidden_size1 = 10  # Number of neurons in the first hidden layer
+    hidden_size2 = 5  # Number of neurons in the second hidden layer
+    num_classes = vocab_size   # Number of output classes
+
+    model = FNNModel(input_size, hidden_size1, hidden_size2, num_classes)
+    print(model)
+    model.load('fnn.pth')
+
+    model.eval()
+    outputs = model(tokens_data.view(1,-1))
+    _, predicted = torch.max(outputs, 1)
+    print(vocab.lookup_token(predicted[0]))
+
+    exit()
+
+    while True:
+        user_input = input("You: ")
+        print("Bot:", user_input)
+        tokens = (tokens + user_input.lower().split())[-context_size:]
+
+        print(tokens, len(tokens))
+    
+        if user_input.lower() == "exit":
+            print("Bot: Goodbye!")
+            break
+
+
+
+chat()
+
+#train()
+
+
 
