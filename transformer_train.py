11f7dd2f7c40eee616980d028eba5b300ed81d3e
Luis A Frigolet <lafrigolet@gmail.com>
2023-08-26 11:16:13 +0200

    Refactoring: getting out train and evaluate function from model

--- transformer_train.py
+++ transformer_train.py
@@ -20,6 +20,72 @@ def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
     data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
     return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
+bptt = 35
+def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+    """
+    Args:
+        source: Tensor, shape ``[full_seq_len, batch_size]``
+        i: int
+
+    Returns:
+        tuple (data, target), where data has shape ``[seq_len, batch_size]`` and
+        target has shape ``[seq_len * batch_size]``
+    """
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].reshape(-1)
+    return data, target
+
+# def train(model: nn.Module, criterion, lr, optimizer, scheduler) -> None:
+def train(model, train_data, criterion, lr, optimizer, scheduler, epoch, ntokens) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 200
+    start_time = time.time()
+        
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        output = model(data)
+        output_flat = output.view(-1, ntokens)
+        loss = criterion(output_flat, targets)
+            
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+            
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+
+def load(model, ptf) -> None:
+    model.load_state_dict(torch.load(ptf)) # load best model states
+
+
+#def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+def evaluate(model, eval_data: Tensor, criterion, ntokens) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i)
+            seq_len = data.size(0)
+            output = model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += seq_len * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+
+
 # ``train_iter`` was "consumed" by the process of building the vocab,
 # so we have to create it again
 #train_iter, val_iter, test_iter = WikiText2()
@@ -75,8 +141,8 @@ with TemporaryDirectory() as tempdir:
 
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        M.train(model, train_data, criterion, lr, optimizer, scheduler, epoch, ntokens)
-        val_loss = M.evaluate(model, val_data, criterion, ntokens)
+        train(model, train_data, criterion, lr, optimizer, scheduler, epoch, ntokens)
+        val_loss = evaluate(model, val_data, criterion, ntokens)
         val_ppl = math.exp(val_loss)
         elapsed = time.time() - epoch_start_time
         print('-' * 89)
@@ -93,7 +159,7 @@ with TemporaryDirectory() as tempdir:
     model.load_state_dict(torch.load(best_model_params_path)) # load best model states
 
 
-test_loss = M.evaluate(model, test_data, criterion, ntokens)
+test_loss = evaluate(model, test_data, criterion, ntokens)
 test_ppl = math.exp(test_loss)
 print('=' * 89)
 print(f'| End of training | test loss {test_loss:5.2f} | '
